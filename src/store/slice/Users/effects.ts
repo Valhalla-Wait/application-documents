4a6c2a437568acd2fa301b8af715dc743ecd5869
Valhalla-Wait <60570103+Valhalla-Wait@users.noreply.github.com>
2022-12-05 17:19:55 +0300

    deploy

--- src/store/slice/Users/effects.ts
+++ src/store/slice/Users/effects.ts
@@ -7,7 +7,6 @@ export const getUsers = createAsyncThunk(
   async (_, { rejectWithValue }) => {
     try {
       const response = await axios.get<UsersType[]>(`${process.env.REACT_APP_BACKEND_URL}/users/get`)
-      console.log(response.data)
       return response.data;
     } catch (e) {
       return rejectWithValue((e as Error).message);
